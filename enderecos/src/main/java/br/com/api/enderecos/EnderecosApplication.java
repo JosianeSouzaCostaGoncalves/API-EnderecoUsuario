37825978d043799e70420db992167857d6121a95
josiane.goncalves <josiane.goncalves@MAGNA-PC.stolabrbhe.net>
2021-04-16 17:09:34 -0300

    Upload inicial

---
+++ enderecos/src/main/java/br/com/api/enderecos/EnderecosApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.enderecos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnderecosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnderecosApplication.class, args);
+	}
+
+}
